05c40500a1665a557edfded96fa74eed824d0f88
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-07-29 14:44:39 -0500

    Missing dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <p className='bg-red-400 text-4xl'>Hola mundo</p>;
+  return (
+    <main className='grid h-screen w-screen place-items-center bg-slate-900'>
+      <h1 className='text-4xl font-bold text-white'>My Form </h1>
+    </main>
+  );
 }
 
 export default App;
